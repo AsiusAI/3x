073e9ca776cbee3066fc0006fea36db84ddb7567
Karel Nagel <nagelkarel@gmail.com>
2025-09-11 18:45:14 +0000

    copy RKPilot imx415 folder

---
+++ imx415/demo.py
@@ -0,0 +1,23 @@
+import cv2
+
+cap = cv2.VideoCapture("/dev/video33")
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('U', 'Y', 'V', 'Y'))
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID') 
+out = cv2.VideoWriter("output.avi", fourcc, 30.0, (1280, 720))
+
+try:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        out.write(frame)
+
+except:
+    print("stopped")
+finally:
+    cap.release()
+    out.release()
