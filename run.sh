94730a326e91472d17be6440de5b933730ba4dde
Karel Nagel <nagelkarel@gmail.com>
2025-09-12 14:35:35 +0000

    imx415 working

---
+++ run.sh
@@ -0,0 +1,8 @@
+dtc -@ -I dts -O dtb -o orangepi-5-max-cam0.dtbo imx415/max/rockchip/overlay/orangepi-5-max-cam0.dts
+
+dtc -@ -I dts -O dtb -o rk3588-orangepi-5-max.dtb imx415/max/rockchip/rk3588-orangepi-5-max.dts
+
+sudo cp orangepi-5-max-cam0.dtbo /usr/lib/firmware/6.1.0-1025-rockchip/device-tree/rockchip/overlay
+sudo cp rk3588-orangepi-5-max.dtb /usr/lib/firmware/6.1.0-1025-rockchip/device-tree/rockchip/
+
+# then run sudo u-boot-update && sudo reboot
